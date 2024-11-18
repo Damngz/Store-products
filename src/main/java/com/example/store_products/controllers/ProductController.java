9693f36044ed0fa23f4cb33f24ed155ee8f0bea1
Damngz <da.munozgz@gmail.com>
2024-11-18 13:47:55 -0300

    feat: docker

--- src/main/java/com/example/store_products/controllers/ProductController.java
+++ src/main/java/com/example/store_products/controllers/ProductController.java
@@ -6,7 +6,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,7 +21,6 @@ import com.example.store_products.service.ProductService;
 
 @RestController
 @RequestMapping("/products")
-@CrossOrigin("http://localhost")
 public class ProductController {
   @Autowired
   private ProductService productService;
